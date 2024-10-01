ef32b2138c0fa12a94767de3f01d57554e8a58a4
Gopala Agrawal <gopalaagrawal@Gopalas-MacBook-Air.local>
2024-10-02 02:18:07 +0530

    chore: explain code and add error middleware

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3001;
+const app: Express = express(); // initialize express app
+const port = process.env.PORT || 3001; // set port
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // implement cors to allow requests from different origins
+app.use(express.json()); // parse incoming requests with JSON payloads e
 
 interface User {
   id: string;
@@ -33,7 +33,7 @@ app.post(
     }
 
     try {
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10); // hash the password using bcrypt this is secure and is one way encryption
       const newUser = {
         id: new Date().toISOString(),
         username,
@@ -42,55 +42,53 @@ app.post(
       users.push(newUser);
       res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
       next(error);
     }
   }
 );
 
-app.post("/login", async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { username, password } = req.body;
+app.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { username, password } = req.body;
 
-  // user will provide username and password and we will send back a token
-  // that token will be used to authenticate the user for further
+    // user will provide username and password and we will send back a token
+    // that token will be used to authenticate the user for further
 
-  if (!username || !password) {
-    res.status(400).json({ message: "Username and password are required" });
-    return;
-  }
-
-  const user = users.find((user) => user.username === username);
-
-  if (!user) {
-    res.status(400).json({ message: "User not found" });
-    return;
-  }
-
-  try {
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      res.status(400).json({ message: "Invalid password" });
+    if (!username || !password) {
+      res.status(400).json({ message: "Username and password are required" });
       return;
     }
 
-    const token = jwt.sign(
-      { id: user.id, username: user.username },
-      process.env.JWT_SECRET as string,
-      { expiresIn: "5m" }
-    );
-    res.status(200).json({ token });
-    return;
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error" });
-    next(error);
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+
+    try {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        res.status(400).json({ message: "Invalid password" });
+        return;
+      }
+
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "5m" }
+      );
+      res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
-  console.log('token: ', token);
   if (!token) {
     res.status(401).json({ message: "Unauthorized" });
     return;
@@ -124,6 +122,12 @@ app.get(
     return;
   }
 );
+
+// using error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
